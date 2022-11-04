e5b34288757bab0862873925526f4f790f1bf128
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-04 16:02:56 +0700

    Create a public void inputdata for insert the minimum and maximu data of array

--- Program.cs
+++ Program.cs
@@ -10,3 +10,21 @@ namespace Exec2
         int n;
         //geet the number of elements to store in the array
         int i;
+
+        public void inputdata()
+        {
+            while (true)
+            {
+                Console.WriteLine("Masukan Nomor untuk elemen di dalam array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 116))
+                    break;
+                else
+                    Console.WriteLine("\n Array harus memiliki minimum 1 dan maximum 116 elemen. \n");
+            }
+        }
+
+    }
+}
+
