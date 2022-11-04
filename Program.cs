f0dc5a00ce0646b7c6f7afe6d847c6ff260dbf64
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-04 16:02:20 +0700

    Create a new variable for a class program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿using System;
+
+namespace Exec2
+{
+    class Program
+    {
+        //Array to be searched
+        int[] ghani = new int[116];
+        //Number of elements in the array
+        int n;
+        //geet the number of elements to store in the array
+        int i;
