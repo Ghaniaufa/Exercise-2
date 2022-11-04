142859a1a79ccad4e63fd90de09b4a7307d5a360
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-04 16:10:07 +0700

    add the Acceptr array elemen

--- Program.cs
+++ Program.cs
@@ -14,16 +14,32 @@ namespace Exec2
         public void inputdata()
         {
             while (true)
-            {
+            {   
+                //Create insert of array
                 Console.WriteLine("Masukan Nomor untuk elemen di dalam array : ");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
                 if ((n > 0) && (n <= 116))
                     break;
                 else
+                    //Create a line for minimum 1 and maximum 116
                     Console.WriteLine("\n Array harus memiliki minimum 1 dan maximum 116 elemen. \n");
             }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("  Enter Array elements ");
+            Console.WriteLine("-----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                ghani[i] = Int32.Parse(s1);
+            }
+
         }
+
+
 
     }
 }
